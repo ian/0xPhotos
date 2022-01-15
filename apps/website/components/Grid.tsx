8cf2ca89b8363995b1a9ea7ce1d6f00da556579e
Ian Hunter <ian@users.noreply.github.com>
2022-01-14 19:50:12 -0600

    Remove commented code

--- apps/website/components/Grid.tsx
+++ apps/website/components/Grid.tsx
@@ -4,11 +4,6 @@ export default function Grid(props) {
   const { elements } = props
 
   return (
-    // <Masonry
-    //   breakpointCols={2}
-    //   // className="my-masonry-grid"
-    //   // columnClassName="my-masonry-grid_column"
-    // >
     <div className="masonry-2-col">
       {elements.map((e, i) => (
         <div className="mb-5" key={`element-${i}`}>
@@ -20,22 +15,5 @@ export default function Grid(props) {
         </div>
       ))}
     </div>
-    // </Masonry>
   )
-  // return (
-  //   <Masonry
-  //     className={'my-gallery-class'} // default ''
-  //     elementType={'ul'} // default 'div'
-  //     options={masonryOptions} // default {}
-  //     disableImagesLoaded={false} // default false
-  //     updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
-  //     //  imagesLoadedOptions={imagesLoadedOptions} // default {}
-  //   >
-  //     {elements.map((e) => (
-  //       <li className="image-element-class">
-  //         <img src={e.url} />
-  //       </li>
-  //     ))}
-  //   </Masonry>
-  // )
 }
