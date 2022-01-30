3a385cce14d341b3d2a6613f35a56de6eec5714c
Ian Hunter <ian@users.noreply.github.com>
2022-01-29 19:26:38 -0600

    homepage popup

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,8 +1,23 @@
+import { Button } from "baseui/button"
+import { IoLogoDiscord } from "react-icons/io5"
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ModalButton,
+  SIZE,
+  ROLE
+} from "baseui/modal"
 import Link from "next/link"
+import { useState } from "react"
 import HowItWorks from "../components/HowItWorks"
 import Layout from "../components/Layout"
+import useWindowDimensions from "../hooks/useWindowDimensions"
 
 export default function Home() {
+  const [isModalOpen, setModalOpen] = useState(false)
+
   return (
     <Layout>
       {/* <h1 className="mb-10 p-40 text-center text-2xl font-bold">Homepage</h1> */}
@@ -23,11 +38,12 @@ export default function Home() {
               </p>
 
               <div className="mt-10 flex space-x-5">
-                <Link href="/">
-                  <a className="bg-black text-white p-4">
-                    Browse the collection
-                  </a>
-                </Link>
+                {/* <Link href="/">
+                  
+                </Link> */}
+                <Button onClick={() => setModalOpen(true)}>
+                  Browse the collection
+                </Button>
                 <Link href="/contributors">
                   <a className="bg-white text-black p-4">
                     Become a contributor
@@ -73,6 +89,79 @@ export default function Home() {
       <div className="py-20">
         <HowItWorks className="max-w-5xl mx-auto" />
       </div>
+
+      <ComingSoonModal
+        isOpen={isModalOpen}
+        onClose={() => setModalOpen(false)}
+      />
     </Layout>
+  )
+}
+
+const ComingSoonModal = ({ isOpen, onClose }) => {
+  const { height, width } = useWindowDimensions()
+
+  return (
+    <Modal
+      onClose={onClose}
+      closeable
+      isOpen={isOpen}
+      animate
+      autoFocus
+      size={SIZE.default}
+      role={ROLE.dialog}
+      overrides={{
+        Dialog: {
+          style: ({ $theme }) => ({
+            borderRadius: 0,
+            padding: 0,
+            width: width * 0.6 + "px",
+            // height: height * 0.8 + "px",
+            overflow: "hidden"
+          })
+        }
+        // DialogContainer: {
+        //   style: () => ({
+        //     padding: 10
+        //   })
+        // }
+      }}
+    >
+      <ModalBody>
+        <div className="p-10 flex flex-col space-y-10 text-xl">
+          <h1 className="text-black font-serif text-4xl">0xPhotos </h1>
+          <p>
+            0xPhotos is a crypto-backed stock photography marketplace that puts
+            photographers first.
+          </p>
+          <p>
+            What if you could keep 95% of your earnings from a sale of your
+            work?
+          </p>
+          <p>
+            We are working hard to make this a possibility but we need your
+            help.
+          </p>
+
+          <div className="flex space-x-5">
+            <Link href="/contributors">
+              <a className="bg-black text-white p-4">
+                Apply to become a contributor
+              </a>
+            </Link>
+
+            <Link href="https://discord.com/invite/rJuvwGQ9YB">
+              <a
+                target="_blank"
+                className="text-white p-4 flex items-center"
+                style={{ background: "#7289DA" }}
+              >
+                <IoLogoDiscord className="mr-2" /> Join us on Discord
+              </a>
+            </Link>
+          </div>
+        </div>
+      </ModalBody>
+    </Modal>
   )
 }
